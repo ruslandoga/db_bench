c6ed56eeb690b7f694f0a6a221595999fe03b2e8
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-12-02 17:43:27 +0700

    remove memory related tweaks

--- lib/app.ex
+++ lib/app.ex
@@ -3,26 +3,21 @@ defmodule App do
   Documentation for `App`.
   """
 
-  # https://pgtune.leopard.in.ua/
+  # based on https://pgtune.leopard.in.ua/
   def pgtune(conn) do
+    workers = System.schedulers_online()
+
     cmds = [
       "ALTER SYSTEM SET max_connections = '100'",
-      "ALTER SYSTEM SET shared_buffers = '3840MB'",
-      "ALTER SYSTEM SET effective_cache_size = '11520MB'",
-      "ALTER SYSTEM SET maintenance_work_mem = '960MB'",
       "ALTER SYSTEM SET checkpoint_completion_target = '0.9'",
-      "ALTER SYSTEM SET wal_buffers = '16MB'",
       "ALTER SYSTEM SET default_statistics_target = '100'",
       "ALTER SYSTEM SET random_page_cost = '1.1'",
       "ALTER SYSTEM SET effective_io_concurrency = '200'",
-      "ALTER SYSTEM SET work_mem = '19660kB'",
       "ALTER SYSTEM SET huge_pages = 'off'",
-      "ALTER SYSTEM SET min_wal_size = '1GB'",
-      "ALTER SYSTEM SET max_wal_size = '4GB'",
-      "ALTER SYSTEM SET max_worker_processes = '4'",
-      "ALTER SYSTEM SET max_parallel_workers_per_gather = '2'",
-      "ALTER SYSTEM SET max_parallel_workers = '4'",
-      "ALTER SYSTEM SET max_parallel_maintenance_workers = '2'"
+      "ALTER SYSTEM SET max_worker_processes = '#{workers}'",
+      "ALTER SYSTEM SET max_parallel_workers_per_gather = '#{max(div(workers, 2), 1)}'",
+      "ALTER SYSTEM SET max_parallel_workers = '#{workers}'",
+      "ALTER SYSTEM SET max_parallel_maintenance_workers = '#{max(div(workers, 2), 1)}'"
     ]
 
     Enum.each(cmds, fn cmd ->
